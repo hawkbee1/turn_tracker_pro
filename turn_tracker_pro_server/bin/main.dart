09a8a31d7a3ed2aa6c6cdd588a8dfb3c52685248
Cirrus CI <support@cirruslabs.org>
2025-12-11 09:58:48 +0000

    initial serverpod creation

---
+++ turn_tracker_pro_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:turn_tracker_pro_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
